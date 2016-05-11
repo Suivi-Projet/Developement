630f29224134c08c227ebf53d6322c4fe4800833
Hannibal974 <romain.poussin@ynov.com>
2016-05-11 23:52:26 +0200

    add ORDER BY previsionnel.php

--- queries/previsionnel.php
+++ queries/previsionnel.php
@@ -7,15 +7,16 @@ if(isset($_GET["idProjet"]) && !is_nan($_GET["idProjet"])) {
 	$reqProjet = $db->prepare("SELECT dateDebutPrevue, dateFinPrevue, SUM(tempsPrevu) as dureePrevu, dateDebutReelle, dateFinReelle,
 									  DATEDIFF(dateDebutReelle, dateDebutPrevue) as ecartDateDebut, DATEDIFF(dateFinReelle, dateFinPrevue) as ecartDateFin,
 									  temp.tempsConsomme, (tempsPrevu-temp.tempsConsomme) as ecartDuree, temp.montantPrevu, temp.montantReel, (temp.montantPrevu-temp.montantReel) as ecartMontant 
-									  FROM taches, (
-										SELECT SUM(tempsPassee) as tempsConsomme, SUM(t.coutPrevu) as montantPrevu, 
-										SUM(c.tempsPassee*r.tauxHoraire) as montantReel 
-										FROM conso as c, taches as t, ressources as r 
-										WHERE c.codeTache = t.codeTache 
-										AND c.codeRessource = r.codeRessource 
-										AND t.codeProjet = ? 
-										GROUP BY c.codeTache
-                                      ) as temp");
+							  FROM taches, (
+								SELECT SUM(tempsPassee) as tempsConsomme, SUM(t.coutPrevu) as montantPrevu, 
+								SUM(c.tempsPassee*r.tauxHoraire) as montantReel 
+								FROM conso as c, taches as t, ressources as r 
+								WHERE c.codeTache = t.codeTache 
+								AND c.codeRessource = r.codeRessource 
+								AND t.codeProjet = ? 
+								GROUP BY c.codeTache
+                              ) as temp
+							  ORDER BY taches.codeTache");
 	$reqProjet->bindParam(1, $_GET["idProjet"]);
 	$reqProjet->execute();
 	$resultProjet = $reqProjet->fetchAll();
@@ -23,16 +24,17 @@ if(isset($_GET["idProjet"]) && !is_nan($_GET["idProjet"])) {
 	$reqTaches = $db->prepare("SELECT t.codeTache, t.libelleTache, f.libelleFamille, t.dateDebutPrevue, t.dateFinPrevue, t.tempsPrevu, 
 								t.dateDebutReelle, t.dateFinReelle, temp.tempsConsomme, temp.cout, ROUND((temp.tempsConsomme/t.tempsPrevu)*100, 1) as ratio,
 								t.avancement, t.tempsPrevu-temp.tempsConsomme as tempsRestant
-						 FROM taches as t, familletache as f,
-						 (
-							SELECT c.codeTache, SUM(c.tempsPassee) as tempsConsomme, SUM(c.tempsPassee*r.tauxHoraire) as cout
-							FROM conso as c, ressources as r
-							WHERE c.codeRessource = r.codeRessource
-							GROUP BY c.codeTache
-						 ) AS temp
-						 WHERE t.codeTache = temp.codeTache    
-						 AND f.codeFamille = t.codeFamille
-						 AND t.codeProjet = ?");
+							   FROM taches as t, familletache as f,
+							   (
+								  SELECT c.codeTache, SUM(c.tempsPassee) as tempsConsomme, SUM(c.tempsPassee*r.tauxHoraire) as cout
+								  FROM conso as c, ressources as r
+								  WHERE c.codeRessource = r.codeRessource
+								  GROUP BY c.codeTache
+							   ) AS temp
+							   WHERE t.codeTache = temp.codeTache    
+							   AND f.codeFamille = t.codeFamille
+							   AND t.codeProjet = ?
+							   ORDER BY t.codeTache");
 	$reqTaches->bindParam(1, $_GET["idProjet"]);	
 	$reqTaches->execute();
 	$resultTaches = $reqTaches->fetchAll();
