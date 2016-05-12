f53b478f39a56a751445e7f73cb5e29bd3e953fa
Hannibal974 <romain.poussin@ynov.com>
2016-05-12 16:03:26 +0200

    POST suivi.php

--- queries/suivi.php
+++ queries/suivi.php
@@ -1,4 +1,55 @@
 <?php
 
 include 'config.php';
+
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+	$json = json_decode(file_get_contents('php://input'), true);
+
+	$req = $db->prepare(INSERT INTO conso (codeRessource, codeTache, date, tempsPassee) VALUES (?, ?, ?, ?));
+	$req->bindParam(1, $json["codeRessource"]);
+	$req->bindParam(2, $json["codeTache"]);
+	$req->bindParam(3, date("Y-m-d"));
+	$req->bindParam(4, $json["tempsPassee"]);
+
+	$done = $req->execute();
+	if(!done)
+		echo json_encode(['codeRetour' => 500, 'result' => "La création de la journée de travail a échouée. Veuillez réessayer dans quelques instants."]);
+	else {
+		$updAvancement = $db->prepare("UPDATE taches SET avancement = ?, dateFinReelle = null WHERE codeTache = ?");
+		$updAvancement->bindParam(1, $json["avancement"]);
+		$updAvancement->bindParam(2, $json["codeTache"]);
+		$doneAvancement = $updAvancement->execute();
+
+		if($doneAvancement) {
+			$reqExistDateDeb = $db->prepare("SELECT dateDebutReelle FROM taches WHERE codeTache = ?");
+			$reqExistDateDeb->bindParam(1, $json["codeTache"]);
+			$reqExistDateDeb->execute();
+			$resultDateDeb = $reqExistDateDeb->fetchAll(PDO::FETCH_ASSOC);
+
+			if(is_null(resultDateDeb["dateDebutReelle"])) {
+				$updTache = $db->prepare("UPDATE taches SET dateDebutReelle = ? WHERE codeTache = ?");
+				$updTache->bindParam(1, date("Y-m-d"));
+				$updTache->bindParam(2, $json["codeTache"]);
+				$doneDateDeb = $updTache->execute();
+
+				if(!$doneDateDeb)
+					echo json_encode(['codeRetour' => 500, 'result' => "La mis à jour de la date de début réelle de la tâche a échouée. Veuillez réessayer dans quelques instants."]);
+
+			}
+
+			if($json["avancement"] == 100) {
+				$updTacheEnd = $db->prepare("UPDATE taches SET dateeFinReelle = ? WHERE codeTache = ?");
+				$updTacheEnd->bindParam(1, date("Y-m-d"));
+				$updTacheEnd->bindParam(2, $json["codeTache"]);
+				$doneUpTacheEnd = $updTacheEnd->execute();
+				if($doneUpTacheEnd)
+					echo json_encode(['codeRetour' => 200, 'result' => null]);
+				else echo json_encode(['codeRetour' => 500, 'result' => "La mise à jour de la date de fin réelle de la tâche a échouée. Veuillez réessayer dans quelques instants."]);
+			}
+		}
+		
+	}
+} else {
+
+}
 ?>
