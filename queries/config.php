1486c3aad9e1551863a78061d8565e9fed81b554
hub3rt <hubert.girondiac@gmail.com>
2016-05-12 14:59:41 +0200

    gn gn gn gn gn

--- queries/config.php
+++ queries/config.php
@@ -1,17 +1,18 @@
+
 <?php
 
-$host = 'localhost';
-$dbname = 'suiviprojet';
-$user = 'root';
-$pass = 'root';
+$host = 'sql7.freesqldatabase.com:3306';
+$dbname = 'sql7119210';
+$user = 'sql7119210';
+$pass = 'VDcmxGmYV9';
 
 try {
 	$db = new PDO('mysql:host='.$host.';dbname='.$dbname, $user, $pass, array(
-    	PDO::ATTR_PERSISTENT => true
+		PDO::ATTR_PERSISTENT => true
 	));
 
 } catch(PDOException $e) {
 	print "Erreur !: " . $e->getMessage() . "<br/>";
-    die();
+	die();
 }
 ?>
